011cae09c19c899b7bc1f9aabf24147a52a509ba
james <dev@dysomni.com>
2022-10-19 00:24:45 -0400

    added remote state to TF

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -2,6 +2,7 @@
 
 source .env
 
+terraform -chdir=tf init -backend-config="bucket=${AWS_LAMBDA_NAME}-tf-state" -backend-config="dynamodb_table=${AWS_LAMBDA_NAME}-tf-lock"
 terraform -chdir=tf apply
 
 rm -r ./build
--- tf/providers.tf
+++ tf/providers.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
