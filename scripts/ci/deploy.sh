5612280b245ba7e40b4f405fc1e5b2a3c67ab658
james <dev@dysomni.com>
2022-10-19 22:30:55 -0400

    removed state deployment for seperate deployment

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -2,7 +2,10 @@
 
 source .env
 
-terraform -chdir=tf init -backend-config="bucket=${AWS_LAMBDA_NAME}-tf-state" -backend-config="dynamodb_table=${AWS_LAMBDA_NAME}-tf-lock"
+terraform -chdir=tf init -migrate-state \
+  -backend-config="bucket=${TF_STATE_S3_BUCKET}" \
+  -backend-config="dynamodb_table=${TF_STATE_DYNAMODB_TABLE}" \
+  -backend-config="key=${AWS_LAMBDA_NAME}/terraform.tfstate"
 terraform -chdir=tf apply
 
 rm -r ./build
--- tf/providers.tf
+++ tf/providers.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
